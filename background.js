3d9081f7815362b91019ab7546dcf3c72418ffc5
unknown <lilyjgeorge@gmail.com>
2016-02-06 18:15:46 -0500

    get extension to kind of work

--- background.js
+++ background.js
@@ -1,18 +1,11 @@
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // No tabs or host permissions needed!
-  console.log('Turning your text black!');
-  chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
-});
-
 /*
  * selectHTML
  * @return a string of text, the background of which will later be changed
  */ 
 function selectHTML() {
     try {
+
+    	console.log("we're starting!");
 
     	// what does this do????
         if (window.ActiveXObject) {
@@ -21,12 +14,18 @@ function selectHTML() {
         }
     
         var selectedText = document.createElement("span");
+        $(selectedText).addClass("blacked_out");
+
         var w = getSelection().getRangeAt(0);
         w.surroundContents(selectedText);
+
+        console.log("you got the text!")
         return selectedText.innerHTML;
 
     // error checking, fuck yeah
     } catch (e) {
+
+    	console.log("something broke");
         if (window.ActiveXObject) {
             return document.selection.createRange();
         } else {
@@ -36,9 +35,36 @@ function selectHTML() {
 }
 
 // change the color of the text returned by selectHTML
-$(function() {
-    $('#changeColor').click( function() {
-        var mytext = selectHTML();
-        $('span').css({"background-color":"black"});
-    });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	console.log('i was clicked!!');
+    var mytext = selectHTML();
+    $('span').css({"background-color":"black"});
 });
+
+
+// function blockAndDisplay() {
+
+
+//     console.log('yolo! I am now running')
+
+
+
+//     $('*::selection').css('background', 'black')
+
+
+
+//     if (feed.length == 0) {
+//         message.remove();
+//     } else if (message.length == 0) {
+//         message = $('<h1>')
+//             .attr('id', 'distracted')
+//             .text("Don't get distracted by Facebook!")
+//             .css('font-size', '30px')
+//             .css('font-family', "'Helvetica Neue', Helvetica, Arial, 'lucida grande', tahoma, verdana, arial, sans-serif")
+//             .css('position', 'relative')
+//             .css('top', '75px');
+//         $('[data-location=maincolumn]').append(message);
+//      }
+
+// }
