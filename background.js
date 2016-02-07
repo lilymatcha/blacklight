affc219110bc8bb98273f00cb87be51ca0dc5e06
unknown <lilyjgeorge@gmail.com>
2016-02-06 19:31:37 -0500

    my baby

--- background.js
+++ background.js
@@ -2,6 +2,5 @@
 // change the color of the text returned by selectHTML
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-	console.log('i was clicked!!');
 	chrome.tabs.executeScript(null, {"file": "contents.js"});
 });
--- contents.js
+++ contents.js
@@ -4,29 +4,22 @@
  */ 
 function selectHTML() {
     try {
-
-    	console.log("we're starting!");
-
-    	// what does this do????
         if (window.ActiveXObject) {
             var c = document.selection.createRange();
             return c.htmlText;
         }
     
         var selectedText = document.createElement("span");
-        // $(selectedText).addClass("blacked_out");
+        $(selectedText).addClass("blacked_out");
 
         var w = getSelection().getRangeAt(0);
         w.surroundContents(selectedText);
 
-        console.log("you got the text!")
         console.log(w);
         return selectedText.innerHTML;
 
-    // error checking, fuck yeah
     } catch (e) {
 
-    	console.log("something broke");
         if (window.ActiveXObject) {
             return document.selection.createRange();
         } else {
@@ -36,4 +29,4 @@ function selectHTML() {
 }
 
  var mytext = selectHTML();
-    $('span').css({"background-color": "#000000"}, {"font-color": "#000000"});
+    $('.blacked_out').css({"background-color": "#000000", "color": "#000000"});
